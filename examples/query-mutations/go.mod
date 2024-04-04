a250dbb99d69e4b5ff541e88214205d5d08e5bd0
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-05 00:45:11 +0100

    Attempted from pprof work

--- examples/query-mutations/go.mod
+++ examples/query-mutations/go.mod
@@ -5,3 +5,8 @@ go 1.22.1
 replace go.trulyao.dev/robin => ../..
 
 require go.trulyao.dev/robin v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	go.trulyao.dev/mirror v0.0.0-20240112230936-5cf9148e332f // indirect
+)
--- examples/query-mutations/main.go
+++ examples/query-mutations/main.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	"net/http/pprof"
+	_ "net/http/pprof"
+
 	"go.trulyao.dev/robin"
 )
+
+const port = 8081
 
 type Error struct {
 	Message string
@@ -37,36 +42,41 @@ var users = []User{
 	{ID: 5, Name: "Charlie"},
 }
 
-func main() {
-	errorHandler := func(err error) ([]byte, int) {
-		message := err.Error()
-		code := 500
+func errorHandler(err error) ([]byte, int) {
+	message := err.Error()
+	code := 500
 
-		if e, ok := err.(Error); ok {
-			code = e.Code
-			message = e.Message
-		} else if e, ok := err.(robin.Error); ok {
-			code = e.Code
-			message = "robin error: " + e.Message
-		}
-
-		return []byte("[via custom handler] " + message), code
+	if e, ok := err.(Error); ok {
+		code = e.Code
+		message = e.Message
+	} else if e, ok := err.(robin.Error); ok {
+		code = e.Code
+		message = "robin error: " + e.Message
 	}
 
-	r := robin.New(&robin.Options{ErrorHandler: errorHandler, EnableDebugMode: true})
+	return []byte("[via custom handler] " + message), code
+}
 
-	r.Add(robin.Query("ping", ping)).
+func main() {
+	r := robin.New(&robin.Options{
+		ErrorHandler:    errorHandler,
+		EnableDebugMode: true,
+	}).
+		Add(robin.Query("ping", ping)).
 		Add(robin.Query("getUser", getUser)).
 		Add(robin.Query("getUsersByIds", getUsersByIds)).
 		Add(robin.Query("getUsers", getUsers)).
 		Add(robin.Mutation("addUser", addUser)).
-		Add(robin.Mutation("deleteUser", deleteUser))
+		Add(robin.Mutation("deleteUser", deleteUser)).
+		Build()
 
 	mux := http.NewServeMux()
 	mux.Handle("POST /_robin", r.Handler())
+	mux.Handle("GET /profile/heap", pprof.Handler("heap"))
+	mux.Handle("GET /profile/trace", pprof.Handler("trace"))
 
-	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", mux)
+	fmt.Printf("Server is running on port %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
 
 	// Or using the default handler, you will have to modify your endpoint to just `/` in the client side
 	//
