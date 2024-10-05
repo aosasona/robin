0d976d85fcfa91ccff3ad3872d1ba21cf3582046
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-10-06 00:04:49 +0100

    Check underlying struct name in ExpectsPayload

--- internal/guarded/check.go
+++ internal/guarded/check.go
@@ -2,12 +2,9 @@ package guarded
 
 import (
 	"reflect"
-
-	"go.trulyao.dev/robin/types"
 )
 
 // ExpectsPayload returns whether the given parameter expects a payload or not by checking if it is a void type
 func ExpectsPayload(param any) bool {
-	void := types.Void{}
-	return !(reflect.TypeOf(param).Kind() == reflect.TypeOf(void).Kind())
+	return reflect.TypeOf(param).Name() != "_RobinVoid"
 }
