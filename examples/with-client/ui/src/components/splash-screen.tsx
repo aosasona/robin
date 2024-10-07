7d558e44ee2d5b99bbff659bccc25d84bdf49b8f
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-10-07 11:31:09 +0100

    Move `with-client` example to separate repo

--- examples/with-client/ui/src/components/splash-screen.tsx
+++
@@ -1,7 +0,0 @@
-export default function SplashScreen() {
-	return (
-		<div class="w-screen h-screen flex items-center justify-center">
-			<p class="animate-bounce">Loading...</p>
-		</div>
-	);
-}
--- examples/with-client/ui/src/lib/client.ts
+++
@@ -1,7 +0,0 @@
-import Client from "./bindings";
-
-const client = Client.new({
-	endpoint: "http://localhost:8081/_robin",
-});
-
-export default client;
