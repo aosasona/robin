905a8f5d34e3e311e619316fe0073f7d10e32c47
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-11-22 03:09:34 +0000

    Added REST API example

---
+++ examples/with-rest-endpoint/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"go.trulyao.dev/robin"
+)
+
+type Todo struct {
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+func main() {
+	r, err := robin.New(robin.Options{
+		EnableDebugMode: true,
+		CodegenOptions: robin.CodegenOptions{
+			Path:             ".",
+			GenerateBindings: false,
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to create a new Robin instance: %s", err)
+	}
+
+	i, err := r.
+		Add(robin.Query("ping", ping)).
+		Add(robin.Query("fail", fail)).
+		Add(robin.Query("list-todos", listTodos)).
+		Add(robin.Mutation("create.todo", createTodo)).
+		Build()
+	if err != nil {
+		log.Fatalf("Failed to build Robin instance: %s", err)
+	}
+
+	if err := i.Serve(
+		robin.ServeOptions{Port: 8060,
+			Route: "/",
+			RestApiOptions: &robin.RestApiOptions{
+				Enable: true,
+			},
+		}); err != nil {
+		log.Fatalf("Failed to serve Robin instance: %s", err)
+		return
+	}
+}
+
+func ping(ctx *robin.Context, _ robin.Void) (string, error) {
+	return "pong", nil
+}
+
+func listTodos(ctx *robin.Context, _ robin.Void) ([]Todo, error) {
+	return []Todo{
+		{"Hello world!", false, time.Now()},
+		{"Hello world again!", true, time.Now()},
+	}, nil
+}
+
+func createTodo(ctx *robin.Context, todo Todo) (Todo, error) {
+	todo.CreatedAt = time.Now()
+	return todo, nil
+}
+
+// Yes, you can just return normal errors!
+func fail(ctx *robin.Context, _ robin.Void) (robin.Void, error) {
+	return robin.Void{}, errors.New("This is a procedure error!")
+}
---
+++ examples/with-rest-endpoint/usage.ts
@@ -0,0 +1,20 @@
+// TODO: call the REST endpoints
+// import Client from "./bindings.ts";
+//
+// const client = Client.new({
+// 	endpoint: "http://localhost:8060",
+// });
+//
+// await client.queries.ping();
+//
+// const todos = await client.queries.todosList();
+// const newTodo = await client.mutations.todosCreate({
+// 	title: "Buy milk",
+// 	completed: false,
+// });
+//
+// console.log("todos -> ", todos);
+// console.log("newTodo -> ", newTodo);
+//
+// // This should throw since the generated client is set to throw on errors
+// await client.queries.fail();
